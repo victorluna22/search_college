0726e08966e421831b74e3158e3a9eddb8092466
Victor Luna <victorluna22@gmail.com>
2013-09-28 21:12:24 -0300

    retirada do celery

--- busca/management/commands/processar_pedidos.py
+++ busca/management/commands/processar_pedidos.py
@@ -1,7 +1,8 @@
 #! -*- coding:utf-8 -*-
 from django.core.management.base import CommandError, BaseCommand
 from busca.models import Relatorio
-from tasks import add
+from busca.crowler import *
+# from tasks import add
 
 class Command(BaseCommand):
 	help = u'Command line para processar os pedidos de busca realizadas no sistema'
@@ -9,8 +10,13 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		relatorios = Relatorio.objects.filter(status_crowler=False)
 		for relatorio in relatorios:
-			print relatorio.id
-			add.delay(relatorio.id, relatorio.universidade, relatorio.de, relatorio.ate)
+			# add.delay(relatorio.id, relatorio.universidade, relatorio.de, relatorio.ate)
+			path_result_crowler = start_crowler(id_relatorio, universidade)
+			path_result_conf = create_file_conf(id_relatorio, path_result_crowler, de, ate)
+			relatorio = Relatorio.objects.get(id=id_relatorio)
+			if relatorio:
+				relatorio.status_crowler = True
+				relatorio.save()
 			
 
 
